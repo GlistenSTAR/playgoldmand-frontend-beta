393dbfcafa3d0fe9964c4dfeefde73045b60c540
Glisten **STAR** <75786284+GlistenSTAR@users.noreply.github.com>
2021-11-03 03:50:07 +0100

    add first splash page

---
+++ components/socials/SocailGroupIcon.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import cb from 'classnames'
+
+const SocailGroupIcon = () => {
+  return (
+    <div className="absolute bottom-10 flex">
+      <div className={cb('mx-1.5')}>
+        <Image
+          src={require('../../assets/img/icons/discord.png')}
+          width="32"
+          height="32"
+          alt="socail_icon"
+        />
+      </div>
+      <div className={cb('mx-1.5')}>
+        <Image
+          src={require('../../assets/img/icons/telegram.png')}
+          width="32"
+          height="32"
+          alt="socail_icon"
+        />
+      </div>
+      <div className={cb('mx-1.5')}>
+        <Image
+          src={require('../../assets/img/icons/reddit.png')}
+          width="32"
+          height="32"
+          alt="socail_icon"
+        />
+      </div> 
+    </div>
+  );
+}
+
+export default SocailGroupIcon;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'landscape':"url('./../assets/img/landscape.png')",
+        'logo_1':"url('./../assets/img/logo_1.png')"
+      }
+    },
   },
   variants: {
     extend: {},
